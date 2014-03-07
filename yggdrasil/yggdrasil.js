e1100c6ca55edc59dd7652389c77dd6c435374cb
samueldaniel <daniel.jacob.samuel@gmail.com>
2014-03-07 16:56:35 -0500

    moved indicies

--- yggdrasil/yggdrasil.js
+++ yggdrasil/yggdrasil.js
@@ -4,8 +4,10 @@ height = 500;
 window.onload = function() {
   var color = d3.scale.category20();
   var encompassing = new Circle(250, 200, 150, 300);
-  var cs = _.sortBy(encompassing.kCircles(8), function (c) { return c.r; }).reverse();
+  var cs = _.sortBy(encompassing.kCircles(10), function (c) { return c.r; }).reverse();
+
   console.log(cs);
+
   var svg = d3.select("#tree")
               .attr("width", width)
               .attr("height", height)
@@ -22,10 +24,7 @@ window.onload = function() {
        .attr("stroke-width", function (d) { return d.s_w; })
        .attr("fill", function (d, i) { return color(i); });
   nodes.append("text")
-       .attr("x", function (d) { return d.cx; })
-       .attr("y", function (d) { return d.cy; })
+       .attr("x", function (d) { return d.cx + d.r * 1.5; })
+       .attr("y", function (d) { return d.cy + d.r * 1.5; })
        .text(function (d, i) { return i;})
-
-
-
 }
