7f398a2b8a47c9ce10bdec47047e621ab9c0e6e7
samueldaniel <daniel.jacob.samuel@gmail.com>
2014-03-07 13:02:20 -0500

    last working commit with svg.js. switching to d3.js

--- yggdrasil/yggdrasil.js
+++ yggdrasil/yggdrasil.js
@@ -2,11 +2,11 @@ window.onload = function() {
   var draw = SVG("tree").size('100%', '100%');
   var encompassing = new Circle(250, 250, 250, 500);
   var cs = encompassing.kCircles(20);
-  var e_c = draw.circle(encompassing.d).attr({
+  /*var e_c = draw.circle(encompassing.d).attr({
     cx: encompassing.cx,
     cy: encompassing.cy,
-    fill: "white"
-  });
+    fill: "grey"
+  });*/
   _.each(cs, function (c) {
       var s_w = c.r / 2;
       draw.circle(c.d).attr({
@@ -17,9 +17,4 @@ window.onload = function() {
         'stroke-width': s_w
       });
   });
-  /*
-  var c = draw.circle(my_circle.d).attr({
-    cx: my_circle.cx,
-    cy: my_circle.cy
-  });*/
 }
